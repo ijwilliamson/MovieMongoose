0747ea3c209b491cd3db13eac1bf134105e9dc10
Ian Williamson <ian@4of.uk>
2022-11-10 17:17:37 +0000

    add search, delete and update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('./db/connection')
 const yargs = require('yargs');
 const mongoose = require('mongoose');
-const {create, read} = require('./movies/function')
+const {create, read, update, deleteMovie, search} = require('./movies/function');
 
 const myApp = async (yargsObject) =>{
 
@@ -9,16 +9,46 @@ const myApp = async (yargsObject) =>{
 
     if (yargsObject.create){
         //code of create
-        await create({title: yargsObject.title, actor: yargsObject.actor, director: yargsObject.director})
+         const result = 
+            await create({title: yargsObject.title,
+                        actor: yargsObject.actor,
+                        director: yargsObject.director});
+        console.log(result)
+        
     } else if (yargsObject.read){
         //code of read
-        await read();
+        console.log(
+            await read()
+        ) ;
+
+    } else if (yargsObject.search) {
+        // code to search for records
+        const result =
+            await search({title: yargsObject.title,
+                          actor: yargsObject.actor,
+                          director: yargsObject.director}
+                        );
+        console.log(result)
+        
+
     } else if (yargsObject.update){
-        //code of update
+        //code of update.
+        const result =
+            await update(yargsObject.id, {title: yargsObject.title,
+                                          actor: yargsObject.actor,
+                                          director: yargsObject.director}
+                                          );
+        console.log(result)
+
     } else if (yargsObject.delete) {
         //code of delete
+        const result = 
+            await deleteMovie(yargsObject.id);
+        console.log(result)
+
     } else {
         console.log('command not recognised')
+
     }
 
     await mongoose.disconnect();
--- src/movies/function.js
+++ src/movies/function.js
@@ -1,17 +1,83 @@
 const MovieCollection = require('./model');
+const {Mongoose} = require('mongoose')
 
 const create = async(movieObject) =>{
     try {
-        const newMovie = await MovieCollection.create(movieObject);
-        console.log(newMovie);
+        const result = await MovieCollection.create(movieObject);
+        return result;
     } catch (error) {
         console.log(error);
     }
 };
 
-const read = async(movieObject) =>{
-    const docs = await MovieCollection.find(); 
-    return docs;
+const read = async() =>{
+    // return an array of objects
+    try {
+        const docs = await MovieCollection.find(); 
+        return docs;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-module.exports = {create, read}
+
+const update = async(id, movieObject) =>{
+    try{
+        if(!id) throw "an id is required"
+
+        const keys = Object.keys(movieObject)
+        const values = Object.values(movieObject)
+        let modifiedMovie = movieObject;
+
+        // loop through the values and remove the key from modifiedMovie
+        // if the value is falsy.
+        for(let i=keys.length; i>=0; i--){
+            if (!values[i]){
+                let {[keys[i]]: unused, ...tempMovie} = modifiedMovie;
+                modifiedMovie = tempMovie;
+            }
+        }
+
+        const result = await MovieCollection.updateOne({_id: id},modifiedMovie)
+        return result;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const search = async(movieObject) =>{
+    try{
+        const keys = Object.keys(movieObject)
+        const values = Object.values(movieObject)
+        let modifiedMovie = movieObject;
+
+        // loop through the values and remove the key from modifiedMovie
+        // if the value is falsy.
+        for(let i=keys.length; i>=0; i--){
+            if (!values[i]){
+                let {[keys[i]]: unused, ...tempMovie} = modifiedMovie;
+                modifiedMovie = tempMovie;
+            }
+        }
+
+        const result = await MovieCollection.find(modifiedMovie)
+        return result;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const deleteMovie = async(id) => {
+    try{
+        if(!id) throw "an id is required"
+
+        const result = await MovieCollection.deleteOne({_id: id})
+        return result;
+    } catch {
+        console.log(error)
+    }
+}
+module.exports = {create, read, update, deleteMovie, search}
+
+// add search
